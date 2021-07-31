aa0b03515dda1922c9cb8039d36e66238a4e6ee8
Chetan Dhangar <chetansdhangar@gmail.com>
2021-07-31 11:54:16 +0530

    test - addtocart and removefrom cart

---
+++ src/shared/products.js
@@ -0,0 +1,104 @@
+export const PRODUCTS = [
+    {
+        id : 1,
+        name : "Palm Royal Wooden Chess Board",
+        image : "",
+        price : 500,
+        isMagnetic : false,
+        isWooden : true,
+        folding : true,
+        descriptiom : ""
+    },
+    {
+        id : 2,
+        name : "YAMAMA folding chess board",
+        image : "",
+        price : 200,
+        isMagnetic : true,
+        isWooden : false,
+        folding : true,
+        descriptiom : ""
+    },
+    {
+        id : 3,
+        name : "MIMI Wooden Chess Board set",
+        image : "",
+        price : 599,
+        isMagnetic : false,
+        isWooden : true,
+        folding : true,
+        descriptiom : ""
+    },
+    {
+        id : 4,
+        name : "Palm Royal Wooden Chess Board",
+        image : "",
+        price : 500,
+        isMagnetic : false,
+        isWooden : true,
+        folding : true,
+        descriptiom : ""
+    },
+    {
+        id : 5,
+        name : "Funskool Games chess set",
+        image : "",
+        price : 215,
+        isMagnetic : false,
+        isWooden : false,
+        folding : false,
+        descriptiom : ""
+    },
+    {
+        id : 6,
+        name : "CHESSCRAFTS Chess set",
+        image : "",
+        price : 4999,
+        isMagnetic : true,
+        isWooden : true,
+        folding : true,
+        descriptiom : ""
+    },
+    {
+        id : 7,
+        name : "Larcy Folding chess board",
+        image : "",
+        price : 499,
+        isMagnetic : false,
+        isWooden : true,
+        folding : true,
+        descriptiom : ""
+    },
+    {
+        id : 8,
+        name : "UPVL Magnetic Chess Board",
+        image : "",
+        price : 599,
+        isMagnetic : true,
+        isWooden : false,
+        folding : true,
+        descriptiom : ""
+    },
+    {
+        id : 9,
+        name : "TUMTUM Magentic Chess Board",
+        image : "",
+        price : 599,
+        isMagnetic : true,
+        isWooden : false,
+        folding : true,
+        descriptiom : ""
+    },
+    {
+        id : 10,
+        name : "StoneCraft Chess Set",
+        image : "",
+        price : 699,
+        isMagnetic : false,
+        isWooden : false,
+        folding : false,
+        descriptiom : ""
+    },
+  
+
+]
